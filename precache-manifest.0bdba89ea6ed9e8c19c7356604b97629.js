847b1c560f8f405a38d35fd433967e0d34eca53c
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-08-01 16:58:28 +0100

    Updates

---
+++ precache-manifest.0bdba89ea6ed9e8c19c7356604b97629.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc44469f8a57d43d0d7eb857a61bc267",
+    "url": "/social-site/index.html"
+  },
+  {
+    "revision": "90d55cc0c2a1a821e951",
+    "url": "/social-site/static/css/2.7c5c0350.chunk.css"
+  },
+  {
+    "revision": "65c23ff9325ca23f09be",
+    "url": "/social-site/static/css/main.4978f02e.chunk.css"
+  },
+  {
+    "revision": "90d55cc0c2a1a821e951",
+    "url": "/social-site/static/js/2.59048e93.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/social-site/static/js/2.59048e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c23ff9325ca23f09be",
+    "url": "/social-site/static/js/main.b1630ba4.chunk.js"
+  },
+  {
+    "revision": "53960e4be967dded1eb8",
+    "url": "/social-site/static/js/runtime-main.0f45afc2.js"
+  },
+  {
+    "revision": "db8223cf9b23e5a22946108f9e8326bf",
+    "url": "/social-site/static/media/error.db8223cf.jpg"
+  }
+]);
